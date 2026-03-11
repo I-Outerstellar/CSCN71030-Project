4595609f83125b863475fa05e4a25f78d7216d6a
Rajdeep Chowdhury <rajdeepchowdhury2007@gmail.com>
2026-03-11 16:06:33 -0400

    Added most board functions

--- Project II Slider Game/Board.cpp
+++ Project II Slider Game/Board.cpp
@@ -1,2 +1,29 @@
 #include "Board.hpp"
 
+Board::Board() {}
+
+bool Board::hasEmptyColumn() const {
+	if (this->board.size() <= 0) return false;
+	for (int i = 0; i < board.size(); i++) {
+		if (board.at(i).size() <= 0) return false;
+	}
+	return true;
+}
+
+std::vector<size_t>& Board::access(size_t column) {
+	return this->board.at(column);
+}
+
+size_t& Board::access(size_t column, size_t row) {
+	return this->access(column).at(row);
+}
+
+bool Board::canAccess(size_t column) const {
+	return (column >= 0 && column < this->board.size());
+}
+
+bool Board::canAccess(size_t column, size_t row) const {
+	if (!canAccess(column)) return false;
+	size_t size = this->board.at(column).size();
+	return (row >= 0 && row < size);
+}
--- Project II Slider Game/Board.hpp
+++ Project II Slider Game/Board.hpp
@@ -23,10 +23,10 @@ public:
 	/// Also checks for if the board itself has no columns.
 	/// </summary>
 	/// <returns>True if a column or the board itself is empty, false if not.</returns>
-	bool hasEmptyColumn();
+	bool hasEmptyColumn() const noexcept;
 
 	/// <summary>
-	/// Checks if a board index can be accessed.
+	/// Checks if a board index can be accessed. Indexing starts at 0.
 	/// </summary>
 	/// <param name="column"></param>
 	/// <param name="row"></param>
@@ -34,24 +34,24 @@ public:
 	bool canAccess(size_t column, size_t row) const noexcept;
 
 	/// <summary>
-	/// Checks if a board column can be accessed.
+	/// Checks if a board column can be accessed. Indexing starts at 0.
 	/// </summary>
 	/// <param name="column"></param>
 	/// <returns>True if column exists, false if not.</returns>
 	bool canAccess(size_t column) const noexcept;
 
 	/// <summary>
-	/// Accesses an integer in the board.
+	/// Accesses an integer in the board. Indexing starts at 0.
 	/// </summary>
 	/// <param name="column"></param>
 	/// <param name="row"></param>
 	/// <returns>Reference to an integer on the board.</returns>
-	int& access(size_t column, size_t row) const;
+	size_t& access(size_t column, size_t row);
 
 	/// <summary>
-	/// Accessses a column of the board.
+	/// Accessses a column of the board. Indexing starts at 0.
 	/// </summary>
 	/// <param name="column"></param>
 	/// <returns>A reference to a column vector of the board.</returns>
-	std::vector<size_t>& access(size_t column) const;
+	std::vector<size_t>& access(size_t column);
 };
