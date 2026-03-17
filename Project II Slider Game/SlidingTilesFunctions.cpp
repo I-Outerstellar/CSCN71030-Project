2d16b93c78073484ff69ae2626a13dd58407ec98
I-Outerstellar <rajdeepchowdhury2007@gmail.com>
2026-03-16 21:58:47 -0400

    Created (somewhat informal) unit tests, fixed existing bugs, wrote the start game function. (#19)
    * Created (somewhat informal) unit tests, fixed existing bugs.
    
    Also, I would really rather not have 0 as a sentinel value for the empty square. It doesn't need to be a sentinel value, after all.
    
    * "hasEmptyRow()" is now uncommented (oops)

--- Project II Slider Game/SlidingTilesFunctions.cpp
+++ Project II Slider Game/SlidingTilesFunctions.cpp
@@ -3,23 +3,33 @@
 
 namespace SlidingTilesFunctions {
 
-	bool slide(Board& board, SlidingTilesEnums::Direction direction) { 
+	void startGame(SlidingTilesEnums::Difficulty difficulty) {
+		SlidingTilesData::board = Board(difficulty);
+		SlidingTilesData::boardSize = static_cast<int>(difficulty);
+		SlidingTilesData::currentColumn =
+			SlidingTilesData::currentRow =
+			SlidingTilesData::boardSize - 1;
+		SlidingTilesData::slides = 0;
+	}
+
+	bool slide(SlidingTilesEnums::Direction direction) { 
 		using namespace SlidingTilesData;
 		bool canSlide = false;
+		if (board.hasEmptyRow()) return false;
 
 		//Determine if a slide can occur in the desired direction
 		switch (direction) {
 		case SlidingTilesEnums::Direction::DOWN:
-			canSlide = board.canAccess(currentColumn + 1, currentRow);
+			canSlide = board.canAccess(currentRow + 1, currentColumn);
 			break;
 		case SlidingTilesEnums::Direction::UP:
-			canSlide = board.canAccess(currentColumn - 1, currentRow);
+			canSlide = board.canAccess(currentRow - 1, currentColumn);
 			break;
 		case SlidingTilesEnums::Direction::LEFT:
-			canSlide = board.canAccess(currentColumn, currentRow - 1);
+			canSlide = board.canAccess(currentRow, currentColumn - 1);
 			break;
 		case SlidingTilesEnums::Direction::RIGHT:
-			canSlide = board.canAccess(currentColumn, currentRow + 1);
+			canSlide = board.canAccess(currentRow, currentColumn + 1);
 			break;
 		}
 
@@ -28,20 +38,20 @@ namespace SlidingTilesFunctions {
 		//Slide the desired square to the empty square
 		switch (direction) {
 		case SlidingTilesEnums::Direction::DOWN:
-			std::swap(board.access(currentColumn, currentRow), board.access(currentColumn + 1, currentRow));
-			currentColumn += 1;
+			std::swap(board.access(currentRow, currentColumn), board.access(currentRow + 1, currentColumn));
+			currentRow += 1;
 			break;
 		case SlidingTilesEnums::Direction::UP:
-			std::swap(board.access(currentColumn, currentRow), board.access(currentColumn - 1, currentRow));
-			currentColumn -= 1;
-			break;
-		case SlidingTilesEnums::Direction::LEFT:
-			std::swap(board.access(currentColumn, currentRow), board.access(currentColumn, currentRow - 1));
+			std::swap(board.access(currentRow, currentColumn), board.access(currentRow - 1, currentColumn));
 			currentRow -= 1;
 			break;
+		case SlidingTilesEnums::Direction::LEFT:
+			std::swap(board.access(currentRow, currentColumn), board.access(currentRow, currentColumn - 1));
+			currentColumn -= 1;
+			break;
 		case SlidingTilesEnums::Direction::RIGHT:
-			std::swap(board.access(currentColumn, currentRow), board.access(currentColumn, currentRow + 1));
-			currentRow += 1;
+			std::swap(board.access(currentRow, currentColumn), board.access(currentRow, currentColumn + 1));
+			currentColumn += 1;
 			break;
 		}
 		return true;
