2c5e4186f843b36e9a986e04a0beac1e08b8026f
RajBhat69 <rbhat@conestogac.on.ca>
2026-04-06 17:24:09 -0400

    Added leaderboard header file

---
+++ GraphicsVersion/LeaderboardScene.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "include/GameObjects.hpp"
+#include "include/GameControllers.hpp"
+#include "../Project II Slider Game/SlidingTilesFunctions.hpp"
+#include "../Project II Slider Game/SlidingTilesData.hpp"
+
+namespace SlidingTilesScenes {
+    namespace LeaderboardScene {
+        // Creates and returns the leaderboard scene
+        GameObjects::GameScene& create();
+
+        // Updates the displayed scores for current difficulty
+        void updateScores();
+
+        // Switches to a different difficulty leaderboard
+        void switchDifficulty(SlidingTilesEnums::Difficulty difficulty);
+    }
+}
