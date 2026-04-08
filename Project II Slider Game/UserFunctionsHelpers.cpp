15972ec0b073996896eac47ff24722e684feb95c
Rajdeep Chowdhury <rajdeepchowdhury2007@gmail.com>
2026-04-08 19:52:43 -0400

    Wrote the UserFunctionsHelpers.hpp/.cpp documentation

--- Project II Slider Game/UserFunctionsHelpers.cpp
+++ Project II Slider Game/UserFunctionsHelpers.cpp
@@ -1,10 +1,17 @@
 ////////////////////////////////////////////////////////////////////////////////
 ///
-/// @file      UserFunctionsHelpers.cpp
-/// @brief     
-/// @details   ~
-/// @author    Rajdeep Chowdhury
-/// @date      8.04.2026
+/// @file		UserFunctionsHelpers.cpp
+/// @brief		File containing the initialization of the @ref UserFunctionsHelpers functions.
+/// @details	This file contains the initialization of the @ref UserFunctionsHelpers helper functions 
+///				declared in @ref UserFunctionsHelpers.hpp.
+/// @author		Rajdeep Chowdhury
+/// @date		8.04.2026
+/// 
+/// @see		UserFunctionsHelpers.hpp
+/// @see		SlidingTilesFunctions.hpp
+///	@see		SlidingTilesData.hpp
+/// @see		SlidingTilesEnums.hpp
+/// @see		UserFunctions
 ///
 ////////////////////////////////////////////////////////////////////////////////
 #include "UserFunctionsHelpers.hpp"
--- Project II Slider Game/UserFunctionsHelpers.hpp
+++ Project II Slider Game/UserFunctionsHelpers.hpp
@@ -18,13 +18,42 @@ namespace SlidingTilesEnums {
 }
 
 namespace UserFunctionsHelpers {
+	/// @brief   Initializes data to start a game of Sliding Tiles.
+	/// @details Uses functions from @ref SlidingTilesFunctions to initialize the data required to start a game
+	///			 of Sliding Tiles.
+	/// @author  Rajdeep Chowdhury
+	/// @date    8.04.2026
 	void startGame();
 
+	/// @brief		Tries to slide the empty tile in the given direction.
+	/// @details	Tries to slide the empty tile in the given directionm handling both the success and failure cases.
+	/// @param[in]	direction The direction the slide will occur.
+	/// @author		Rajdeep Chowdhury
+	/// @date		8.04.2026
 	void trySlide(SlidingTilesEnums::Direction direction);
 
+	/// @brief   Print the Sliding Tiles board.
+	/// @details Displays all the tiles of the Sliding Tiles board onto the console.
+	/// @author  Rajdeep Chowdhury
+	/// @date    8.04.2026
 	void printBoard();
 
+	/// @brief   Print the Sliding Tiles leaderboard for the current difficulty.
+	/// @details Displays the top @ref SlidingTilesFunctionsHelpers::LEADERBOARD_SIZE scores achieved for the
+	///			 @ref SlidingTilesData::currentDifficulty being played.
+	/// @author  Rajdeep Chowdhury
+	/// @date    8.04.2026
+	/// 
+	/// @see	 SlidingTilesFunctionsHelpers
+	/// @see	 SlidingTilesData
 	void printLeaderboard();
 
+	/// @brief   Determines whether the Sliding Tiles game should continue.
+	/// @details Determines whether the Sliding Tiles game should continue based on if the board is ordered or not.
+	///			 If the board is ordered, it will also handle applying the effect of winning the game.
+	/// @retval	 true If the game must still continue.
+	/// @retval	 false If the game should end.
+	/// @author  Rajdeep Chowdhury
+	/// @date    8.04.2026
 	bool continueGame();
 }
